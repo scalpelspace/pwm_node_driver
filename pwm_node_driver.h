7681bde068c5df777aa3bd817092b74d9795739f
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2025-12-08 11:48:56 -0500

    Refactor and implement abstracted CAN drivers

---
+++ pwm_node_driver.h
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * @file pwm_node_driver.h
+ * @brief PWM Node driver for CAN based communication.
+ *******************************************************************************
+ */
+
+#ifndef PWM_NODE__DRIVER_H
+#define PWM_NODE__DRIVER_H
+
+/** Includes. *****************************************************************/
+
+#include "can_driver/can_driver.h"
+#include "pwm_node_can_dbc.h"
+
+#endif
