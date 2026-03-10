30ca88d9ea6e2422559a90f1d351af9716454f57
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-03-10 02:27:10 -0400

    Update can_driver (prerel) and update DBC for RTC

--- pwm_node_can_dbc.h
+++ pwm_node_can_dbc.h
@@ -17,10 +17,24 @@
 extern "C" {
 #endif
 
+/** DBC message index enum. **************************************************/
+
+typedef enum {
+  PWM_NODE_CAN_DBC_IDX_STATE = 0,
+  PWM_NODE_CAN_DBC_IDX_COMMAND_SERVO_1 = 1,
+  PWM_NODE_CAN_DBC_IDX_COMMAND_SERVO_2 = 2,
+  PWM_NODE_CAN_DBC_IDX_COMMAND_SERVO_3 = 3,
+  PWM_NODE_CAN_DBC_IDX_COMMAND_SERVO_4 = 4,
+  PWM_NODE_CAN_DBC_IDX_DATETIME_SET = 5,
+  PWM_NODE_CAN_DBC_IDX_DATETIME_GET = 6,
+  PWM_NODE_CAN_DBC_IDX_DATETIME_GET_RESPONSE = 7,
+
+  PWM_NODE_CAN_DBC_IDX_COUNT // Total message count.
+} pwm_node_can_dbc_index_t;
+
 /** Public variables. *********************************************************/
 
 extern const can_message_t dbc_messages[];
-extern const int dbc_message_count;
 
 /** CPP guard close. **********************************************************/
 
